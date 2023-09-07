06414e02633450187b87d9e8f7be1935754050e9
Alexander Broll <alexbroll1302@gmail.com>
2023-09-07 13:57:02 +0000

    new shapenet temp path

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
-HOME=`pwd`
+HOME="$(pwd)"
 
 # Chamfer Distance
-cd $HOME/extensions/chamfer_dist
+cd "$HOME"/extensions/chamfer_dist
 python setup.py install --user
 
 # NOTE: For GRNet 
